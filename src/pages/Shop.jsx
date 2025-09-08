5e3653c615e1bcc8bf8071bfe56df558de3fd6d0
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-08 21:01:55 +0530

    Set up a non functional product grid

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react"
+import './Shop.css'
 function Shop() {
-
+  const [product, setProduct]=useState([]);
+  useEffect(()=>{
+    const fetchData= async ()=>{
+      try{const response=await fetch('https://fakestoreapi.com/products')
+      const data=await response.json();
+      setProduct(data);} catch(error){console.log(`Error: ${error}`)}
+    };
+    fetchData();
+  },[]);
   return (
-    <div>
-      Shop
+    <div className="product-grid">
+      {
+        product.map((item)=>(
+          <div key={item.id} className="product">
+            <div className="image"><img src={item.image} alt={item.title} width='170px' height='170px'/></div>
+            <div className="text">
+              <div>{item.title}</div>
+              <div>${item.price}</div>
+            </div>
+          </div>
+        ))
+      }
     </div>
-  )
+  );
 }
 
 export default Shop
