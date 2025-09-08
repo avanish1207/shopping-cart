ef66bfcc404200f602d4b3d4423e99d2252af1f4
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-08 16:10:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Cart from './pages/Cart'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
