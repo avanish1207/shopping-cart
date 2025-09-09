149014227cb0085cf88d347703bb26a8751369d9
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-10 00:20:01 +0530

    Function to add items to the card

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,6 +5,19 @@ const CartContext=createContext();
 export const CartProvider=({children})=>{
     const [cartItems, setCartItems]=useState([]);
 
+    const addToCart=(product, quantity=1){
+        setCartItems(prevItems=>{
+            const existingItem=prevItems.find(item=>item.id===product.id);
+            if(existingItem){
+                return prevItems.map(item=>
+                    item.id===product.id?{...item,quantity:item.quantity+quantity}:item
+                )
+            } else{
+                return [...prevItems, {...product, quantity}];
+            }
+        })
+    }
+
     const value={
         cartItems, setCartItems
     };
